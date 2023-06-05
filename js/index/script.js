7a67b7a5c23954dfe272f50e5537ffee4c2d387f
Reedus0 <lazsasha2006@gmail.com>
2023-06-05 18:36:13 +0300

    initial commit

---
+++ js/index/script.js
@@ -0,0 +1,28 @@
+body = document.body;
+
+window.onload = function () {
+    document.addEventListener("click", documentActions);
+
+    function documentActions(e) {
+        const targetElement = e.target;
+        if (targetElement.classList.contains('burger-header')) {
+            console.log(true)
+            body.classList.toggle('_menu');
+            document.querySelector('.burger-header__burger').classList.toggle('_active')
+            document.querySelector('.header__drop').classList.toggle('_active')
+        }
+    }
+
+}
+
+const downloadFile = () => {
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', 'app/app-release.apk');
+    anchor.setAttribute('download', '');
+    document.body.appendChild(anchor);
+    anchor.click();
+    anchor.parentNode.removeChild(anchor);
+}
+
+
+
